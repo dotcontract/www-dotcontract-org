cd1f63f4da3da3412960bb3d6faacaa952a5c859
Foy Savas <foy@sav.as>
2023-01-01 21:27:27 -0500

    some text

--- app/head.mjs
+++ app/head.mjs
@@ -3,7 +3,9 @@ import { getLinkTag } from "@enhance/arc-plugin-styles/get-styles";
 export default function Head(state) {
   const { store, status, req, error } = state;
   const { path } = req;
-  const title = store?.pageTitle ? `DotContract — ${store?.pageTitle}` : `DotContract`;
+  const title = store?.pageTitle
+    ? `DotContract — ${store?.pageTitle}`
+    : `DotContract`;
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -13,6 +15,10 @@ export default function Head(state) {
       <title>${title}</title>
       ${getLinkTag()}
       <link rel="icon" href="/_public/favicon.svg">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Sanchez&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Inter&display=swap" rel="stylesheet">
     </head>
   `;
 }
