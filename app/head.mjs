95a3ef35948dfe48b3d849cfcc146c3dcae3a398
Foy Savas <foy@sav.as>
2023-01-06 19:45:06 -0500

    mobile styles

--- app/head.mjs
+++ app/head.mjs
@@ -20,6 +20,7 @@ export default function Head(state) {
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Hepta+Slab:wght@400;500&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet">
     </head>
   `;
