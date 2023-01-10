2ea3809fad25883f941edf88ff3ecf3c027daa1f
Foy Savas <foy@sav.as>
2023-01-10 15:07:42 -0500

    add dotc log example

--- app/head.mjs
+++ app/head.mjs
@@ -22,6 +22,7 @@ export default function Head(state) {
       <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css2?family=Hepta+Slab:wght@400;500&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Hepta+Slab:wght@400;500&family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     </head>
   `;
 }
